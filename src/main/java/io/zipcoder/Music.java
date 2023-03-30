f127fb202106ec2105706dc1119562f47154361f
dayvidtran <dayvidtran@gmail.com>
2023-03-29 22:32:04 -0400

    loopy loop forever the playlist

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,30 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int nextSong = 0;
+        int lastSong = 0;
+        for (int i  = startIndex; i<= playList.length; i++){
+            if (i== playList.length){
+                i=0;
+            }
+            nextSong++;
+            if (playList[i] == selection){
+                break;
+            }
+        }for (int j = startIndex; j>-1; j--){
+            if (j == -1){
+                j = playList.length -1;
+            }
+            lastSong++;
+            if (playList[j] == selection){
+                break;
+            }
+        }
+        if (nextSong < lastSong){
+            return nextSong;
+        }else {
+            return lastSong;
+        }
+
     }
 }
